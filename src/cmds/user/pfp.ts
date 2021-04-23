9ed94c871bb2363f33e84d525e9f8fb7e3d03422
fer <afriguez@gmail.com>
2021-04-23 04:51:59 -0500

    fix(cmds): Low avatar quality
    Increase avatar quality and add footer with message author's info

--- src/cmds/user/pfp.ts
+++ src/cmds/user/pfp.ts
@@ -7,10 +7,17 @@ export let help = cmdsHelp.pfp;
 
 exports.run = async (msg: Message) => {
   let member = msg.mentions.members!.first() || msg.member;
-
   let embed = new MessageEmbed()
-    .setImage(member!.user.displayAvatarURL())
-    .setColor("RANDOM");
+    .setImage(
+      member!.user.displayAvatarURL({
+        format: "png",
+        dynamic: true,
+        size: 1024,
+      })!
+    )
+    .setColor("RANDOM")
+    .setFooter(msg.author.username, msg.author.displayAvatarURL())
+    .setTimestamp();
 
   msg.channel.send(embed);
 };
