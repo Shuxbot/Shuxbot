3918e66c9a512df7e78a118887d46834fd6b5c61
Fer <afriguez@gmail.com>
2021-09-29 14:13:39 -0500

    fix(cmds): Using name instead of ID

--- src/cmds/user/color.ts
+++ src/cmds/user/color.ts
@@ -10,29 +10,29 @@ import { getLevelByPoints } from "../../util/utils";
 let help = cmdsHelp.color;
 
 exports.run = async (msg: Message, args: string[]) => {
-  let name = args.join(" ");
+  let name = args.join("");
 
-  if (!name) return msg.reply("debes especificar que rol\n" + help.usage);
+  if (!name) return msg.reply("debes especificar el color\n" + help.usage);
 
   let sUser = new ShuxUser(msg.author);
   let uData = await sUser.get();
 
   for (let color in colors) {
-    if (name == color) {
+    if (name == colors[color].name) {
       if (colors[color].level <= Math.floor(getLevelByPoints(uData.points))) {
         let lastColor = uData.color;
 
         db.ref(sUser.ref)
-          .update({ color: colors[color].id })
+          .update({ color: color })
           .then(() => {
-            if (!msg.guild!.roles.cache.has(colors[color].id)) {
+            if (!msg.guild!.roles.cache.has(color)) {
               return msg.reply("oops! no existe el color en el servidor!");
             }
 
             if (msg.guild!.roles.cache.has(uData.color)) {
               msg.member!.roles.remove(lastColor);
             }
-            msg.member!.roles.add(colors[color].id);
+            msg.member!.roles.add(color);
             msg.reply("se ha actualizado su color!");
           });
       }
