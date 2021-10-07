33cfbfec727022058cdd8b077d312a0804028a47
Fer <afriguez@gmail.com>
2021-10-07 11:57:12 -0500

    feat(cmds): Search pfp by uid

--- src/cmds/user/pfp.ts
+++ src/cmds/user/pfp.ts
@@ -1,12 +1,25 @@
 import { Message, MessageEmbed } from "discord.js";
+import { log } from "../../config/config";
 
 // Source imports
 import { cmdsHelp } from "../../util/cmdsHelp";
+import { getopts, getValue } from "../../util/utils";
 
 export let help = cmdsHelp.pfp;
 
-exports.run = async (msg: Message) => {
-  let member = msg.mentions.members!.first() || msg.member;
+exports.run = async (msg: Message, args: string[]) => {
+  let opts = getopts(args, { u: "--user" });
+  let userId = getValue(opts, "user");
+
+  let member: any = msg.mentions.members!.first() || msg.member;
+  if (userId)
+    member = await msg.guild!.members.fetch(userId).catch((err) => {
+      log.error(err.message);
+      return undefined;
+    });
+
+  if (!member)
+    return msg.reply("El usuario no esta en el servidor o la ID es invalida");
   let embed = new MessageEmbed()
     .setImage(
       member!.user.displayAvatarURL({
